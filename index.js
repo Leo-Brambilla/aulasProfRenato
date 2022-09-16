598eb2ba9efb0016a070ae5270a82dbaf49513c7
Leo-Brambilla <leo.a.brambilla@gmail.com>
2022-09-16 17:15:26 -0300

    Aulas Prof Renato

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+    * Tipos primitivos
+    * Variáveis no JS
+    * Operadores matemáticos em JS    
+ */
+
+/** Tipos primitivos
+    * string - textos
+    * number - números
+    * boolean - booleano (true/false or 0 1)
+ */
+console.log('Olá Mundo!') // serve para escrever mensagem no terminal
+console.log(5)
+console.log(43.76)
+console.log(true)
+console.log(false)
+
+/**Variáveis 
+ * let - pegue
+ * var - variável
+*/
+
+let mensagem = "a vida é boa"
+console.log(mensagem)
+
+let a = 5
+let b = 7
+let tot = a + b
+console.log(`Soma de a=${a} e b=${b} é igual a ${tot} e ${mensagem}`)
+
+
+
